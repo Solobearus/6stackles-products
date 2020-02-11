7595464448a710769624d68b40a0d97ec277a67a
naorpeled <thehecticbyte@gmail.com>
2020-02-11 14:42:45 +0200

    fixed routes - product routes now recieve id after token verification only and not product author directly

--- controllers/product/create.js
+++ controllers/product/create.js
@@ -1,8 +1,30 @@
 const Product = require('../../models/product');
 
-exports.createProduct = (req, res) => {
-    const product = new Product(req.body);
+exports.createProduct = async (req, res) => {
+
     
+    const {
+        name,
+        description,
+        price,
+        condition,
+        category,
+        location,
+        userId: author
+    } = req.body;
+
+    const params = {
+        name,
+        description,
+        price,
+        condition,
+        category,
+        location,
+        author
+    }
+
+    const product = new Product(params);
+
     product.save((err, user) => {
         if (err) {
             return res.status(400).json({ err });
