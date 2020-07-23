97a0ff77bc7cae9080f1f54592bd3402a7195935
Ivan Solobear <ivansolobear@gamil.com>
2020-07-23 14:51:48 +0300

    change ACL 2 public read

--- controllers/product/create.js
+++ controllers/product/create.js
@@ -12,6 +12,7 @@ const s3 = new aws.S3({
 const uploadS3 = multer({
     storage: multerS3({
         s3: s3,
+        acl: 'public-read',
         bucket: process.env.S3_BUCKET,
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
